7839037ca7359b6b3cbced5394e2677526e509f5
Sean Carr Geoffrey <54399662+SeanCarr1@users.noreply.github.com>
2026-04-04 08:16:50 +0800

    Refactor BMI calculator assembly program for improved structure and clarity

--- selection.asm
+++ selection.asm
@@ -1,112 +1,141 @@
-.386
-.model flat, stdcall
-
+.model small
+.stack 100h
 
 ; BMI CALCULATOR - ASSEMBLY PROGRAM
 ; Topics: Selection Statement, Array, Loop
 
-
 .data
-    ; Data segment - Define arrays and constants
-    weights DWORD 50, 70, 90, 60, 120       ; Array of 5 weights
-    height DWORD 170                        ; Fixed height
-    arrayLength DWORD 5                     ; Array size
-    
-    ; Constants for BMI classification
-    obese_threshold DWORD 30
-    overweight_threshold DWORD 25
-    normal_threshold DWORD 18
-    
-    ; Strings for output
-    header_msg BYTE "BMI Classification:", 0Ah, 0Dh, 0
-    weight_msg BYTE "Weight: ", 0
-    bmi_msg BYTE "kg -> BMI: ", 0
-    obese_str BYTE "Obese", 0
-    overweight_str BYTE "Overweight", 0
-    normal_str BYTE "Normal", 0
-    underweight_str BYTE "Underweight", 0
-    newline BYTE 0Ah, 0Dh, 0
+    weights db 50,70,90,60,120
+    bmi db 0
+
+    header_msg db 'BMI Classification:',13,10,'$'
+    weight_msg db 'Weight: $'
+    bmi_msg db ' -> BMI: $'
+    obese_str db 'Obese',13,10,'$'
+    overweight_str db 'Overweight',13,10,'$'
+    normal_str db 'Normal',13,10,'$'
+    underweight_str db 'Underweight',13,10,'$'
 
 .code
-main PROC
-    ; Print header message
-    ; mov eax, 0
-    ; mov ebx, 1
-    ; mov ecx, header_msg
-    ; mov edx, 18
-    ; int 80h
-    
-    ; Initialize registers for loop
-    MOV ECX, 0                              ; ECX = loop counter (i = 0)
-    
-LOOP_START:
-    ; Check if i < arrayLength (i < 5)
-    CMP ECX, 5                              ; Compare ECX with 5
-    JGE LOOP_END                            ; Jump if Greater or Equal (end loop)
-    
-    ; Calculate array index: weights[i]
-    MOV EAX, [weights + ECX * 4]            ; EAX = weights[i]
-    MOV EBX, EAX                            ; EBX = current weight
-    
-    ; Calculate BMI = weight / (height * height / 10000)
-    ; For simplicity: approximate BMI calculation
-    MOV EAX, EBX                            ; EAX = weight
-    MOV EDX, 20                             ; EDX = approximate divisor
-    DIV EDX                                 ; EAX = weight / 20 (approximation)
-    
-    ; EAX now contains approximated BMI
-    MOV ESI, EAX                            ; ESI = BMI value
-    
-    
-    ; SELECTION STATEMENT - Classify BMI
-    
-    
-    ; Check if BMI >= 30 (Obese)
-    CMP ESI, 30
-    JGE IS_OBESE
-    
-    ; Check if BMI >= 25 (Overweight)
-    CMP ESI, 25
-    JGE IS_OVERWEIGHT
-    
-    ; Check if BMI >= 18 (Normal)
-    CMP ESI, 18
-    JGE IS_NORMAL
-    
-    ; Else Underweight
-    JMP IS_UNDERWEIGHT
-    
-IS_OBESE:
-    MOV EDI, OFFSET obese_str
-    JMP PRINT_RESULT
-    
-IS_OVERWEIGHT:
-    MOV EDI, OFFSET overweight_str
-    JMP PRINT_RESULT
-    
-IS_NORMAL:
-    MOV EDI, OFFSET normal_str
-    JMP PRINT_RESULT
-    
-IS_UNDERWEIGHT:
-    MOV EDI, OFFSET underweight_str
-    
-PRINT_RESULT:
-    ; Print: "Weight: " followed by weight value
-    ; (In actual DOS, use INT 21h)
-    ; For assembly simulation:
-    ; Print weight and category
-    ; Using CALL to hypothetical print function
-    
-    ; Increment loop counter
-    INC ECX
-    JMP LOOP_START
-    
-LOOP_END:
-    ; Program end
-    MOV EAX, 0                              ; Return 0
-    RET
-    
-main ENDP
+start:
+    mov ax,@data
+    mov ds,ax
 
-END main
+    lea dx,header_msg
+    mov ah,09h
+    int 21h
+
+    xor si,si
+    mov cl,5
+
+loop_start:
+    mov al,weights[si]
+    xor ah,ah
+    mov bl,2
+    div bl
+    mov bmi,al
+
+    lea dx,weight_msg
+    mov ah,09h
+    int 21h
+
+    mov al,weights[si]
+    call print_num_al
+
+    lea dx,bmi_msg
+    mov ah,09h
+    int 21h
+
+    mov al,bmi
+    cmp al,30
+    jae is_obese
+
+    cmp al,25
+    jae is_overweight
+
+    cmp al,18
+    jae is_normal
+
+    jmp is_underweight
+
+is_obese:
+    lea dx,obese_str
+    mov ah,09h
+    int 21h
+    jmp next_item
+
+is_overweight:
+    lea dx,overweight_str
+    mov ah,09h
+    int 21h
+    jmp next_item
+
+is_normal:
+    lea dx,normal_str
+    mov ah,09h
+    int 21h
+    jmp next_item
+
+is_underweight:
+    lea dx,underweight_str
+    mov ah,09h
+    int 21h
+
+next_item:
+    inc si
+    loop loop_start
+
+    mov ah,4ch
+    int 21h
+
+print_num_al proc
+    push ax
+    push bx
+    push cx
+    push dx
+
+    xor ah,ah
+    mov bl,100
+    div bl
+    mov bh,al
+
+    mov al,ah
+    xor ah,ah
+    mov bl,10
+    div bl
+    mov ch,al
+    mov cl,ah
+
+    cmp bh,0
+    je skip_hundreds
+    mov dl,bh
+    add dl,30h
+    mov ah,02h
+    int 21h
+
+skip_hundreds:
+    cmp bh,0
+    jne print_tens
+    cmp ch,0
+    je skip_tens
+
+print_tens:
+    mov dl,ch
+    add dl,30h
+    mov ah,02h
+    int 21h
+
+skip_tens:
+    mov dl,cl
+    add dl,30h
+    mov ah,02h
+    int 21h
+
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+print_num_al endp
+
+end start
